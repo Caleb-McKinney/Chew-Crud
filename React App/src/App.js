641cfcd89371a80ac6216f81d566e40377145f5a
Caleb McKinney <McKinney.Caleb@gmail.com>
2024-06-09 20:13:01 -0500

    aligning files and folders

--- React App/src/App.js
+++ React App/src/App.js
@@ -1,34 +1,25 @@
-/*
- * Real time private chatting app using React, Nodejs, mongodb and Socket.io
- * @author Shashank Tiwari
- */
-
-import React, { Component } from 'react';
-
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from "react-router-dom";
-
-import Authentication from './pages/authentication/Authentication';
-import Home from './pages/home/Home';
-import NotFound from './pages/not-found/NotFound';
-
+import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Authentication} />
-          <Route path="/home/" component={Home} />
-          <Route component={NotFound} />
-        </Switch>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
