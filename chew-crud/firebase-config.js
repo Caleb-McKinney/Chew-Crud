ef72aff99198ec28b482f1e0237fa465e8bcef32
Caleb McKinney <McKinney.Caleb@gmail.com>
2024-06-09 19:15:22 -0500

    adding authentication

--- chew-crud/firebase-config.js
+++ chew-crud/firebase-config.js
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDffI6QJzlASSBUPa14_RqZ_H8HOxoKTDQ",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "chew-crud.firebaseapp.com",
   projectId: "chew-crud",
   storageBucket: "chew-crud.appspot.com",
   messagingSenderId: "339739151201",
-  appId: "1:339739151201:web:3ea58ef42f12899cc4d320",
+  appId: process.env.REACT_APP_FIREBASE_API_ID,
   measurementId: "G-TSKWY0R8QS"
 };
 
